bbc4572693b02525f46397cea64fb06f21c2deef
Emmanuel Ihejirika <167711770+Spotlight99@users.noreply.github.com>
2026-05-31 22:31:44 +0100

    Update Layout.js
    Trying to make background image take up the entire home page

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 
-
-export default function Layout ({ children }) {
+export default function Layout({ children }) {
     return (
-        <div className="min-h-screen bg-black text-white flex flex-col">
+        <div
+            className="min-h-screen text-white flex flex-col bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: "url('/hero.jpg')" }}
+        >
             <Navbar />
             <main className="flex-grow">{children}</main>
             <Footer />
