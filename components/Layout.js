968005a51396837c674e14ae6119e605242158f4
Emmanuel Ihejirika <167711770+Spotlight99@users.noreply.github.com>
2026-05-31 22:44:37 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,13 +3,21 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
     return (
-        <div
-            className="min-h-screen text-white flex flex-col bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: "url('/hero.jpg')" }}
-        >
-            <Navbar />
-            <main className="flex-grow">{children}</main>
-            <Footer />
+        <div className="relative min-h-screen text-white flex flex-col">
+
+            <div
+                className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+                style={{ backgroundImage: "url('/hero.jpg')" }}
+            />
+
+            <div className="absolute inset-0 bg-black/70" />
+
+            <div className="relative z-10 flex flex-col min-h-screen">
+                <Navbar />
+                <main className="flex-grow">{children}</main>
+                <Footer />
+            </div>
+
         </div>
     )
 }
